9ab6dbc2ed8e82531e4717214d2f3613b4874c84
Ndibnu Christian <ndibnuchristian61@gmail.com>
2022-09-05 11:36:33 +0000

    version2

--- deploy.py
+++ deploy.py
@@ -5,3 +5,5 @@ print ('Hello world')
 #= python script
 # thank you lord for always being there for me i will always praise and worshiped you, may your name be exhauted and glorified always
 
+
+print('Hello World')
